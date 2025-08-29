92372e14ecdf30817cfee7f50db407b6394e8e04
livianabelly <livianabelly.caveden@gmail.com>
2025-08-28 21:51:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function buscar() {
+    //alert("okk")
+
+    //funçao q carrega as informações do JSON
+    fetch('https://raw.githubusercontent.com/livianabelly/minhaAPI/refs/heads/main/dados.json').then(resposta => resposta.json()).then(banco =>{
+
+        var identificador = document.getElementById('valorDigitado').value 
+        var encontrado = false;
+
+        banco.forEach(pessoa => {
+            if (identificador == pessoa.cpf) {
+                //JQUERY
+                var linkSite = `<a href="${pessoa.link}"target="_blank">Visite no site</a>`;
+                document.getElementById('imagem').innerHTML = pessoa.imagem
+                document.getElementById('nome').innerHTML = pessoa.nome
+                document.getElementById('sobrenome').innerHTML = pessoa.sobrenome
+                document.getElementById('cidade').innerHTML = pessoa.cidade
+                document.getElementById('pais').innerHTML = pessoa.pais
+                document.getElementById('link').innerHTML = linkSite;
+                document.getElementById('erro').innerHTML = ""
+                encontrado = true
+            }
+        })
+
+        if (!encontrado) {
+            document.getElementById('erro').innerHTML = "CPF não encontrado";
+        }
+        console.log(banco)
+    })
+}
